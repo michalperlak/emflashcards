c3fa53b13f1ca1c24836b25fa6911dfb0e36fe19
Michal Perlak <michalperlak@windowslive.com>
2024-04-02 15:53:30 +0200

    Update query logic

--- backend/src/main/kotlin/com/github/michalperlak/flashcards/cards/application/CardsQueryService.kt
+++ backend/src/main/kotlin/com/github/michalperlak/flashcards/cards/application/CardsQueryService.kt
@@ -1,20 +1,31 @@
 package com.github.michalperlak.flashcards.cards.application
 
 import com.github.michalperlak.flashcards.cards.model.Card
+import com.github.michalperlak.flashcards.cards.model.UserLearningState
 import com.github.michalperlak.flashcards.cards.repository.CardsRepository
+import com.github.michalperlak.flashcards.time.TimeService
 import com.github.michalperlak.flashcards.users.model.UserId
 import com.github.mpps.fsrs.model.State
 import org.springframework.stereotype.Service
+import java.time.LocalTime
+import java.time.OffsetDateTime
 
 @Service
 class CardsQueryService(
-    private val cardsRepository: CardsRepository
+    private val cardsRepository: CardsRepository,
+    private val timeService: TimeService
 ) {
     fun getForToday(userId: UserId, states: Set<State>, limit: Int): List<Card> =
         cardsRepository
             .getAll()
             .distinctBy { it.id }
-            .filter { states.contains(it.learningState[userId].state) }
+            .filter { states.contains(it.learningState[userId].state) && isForNextReview(it.learningState[userId]) }
             .sortedBy { it.learningState[userId].due }
             .take(limit)
+
+    private fun isForNextReview(learningState: UserLearningState): Boolean {
+        val now = timeService.getCurrentTimestamp()
+        val midnight = OffsetDateTime.of(now.toLocalDate(), LocalTime.MIDNIGHT, now.offset)
+        return learningState.due.isBefore(midnight.plusDays(7))
+    }
 }
